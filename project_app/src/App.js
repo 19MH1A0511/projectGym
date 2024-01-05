1af5d4179ff5621a922672ec6cbb98bf1cc90c92
19MH1A0511 <chittidiganesh999@gmail.com>
2024-01-06 00:53:30 +0530

    commit

--- project_app/src/App.js
+++ project_app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Hero from './compoments/Hero/Hero';
 import Header from './compoments/Hero/Header/Header';
 import Program from './compoments/Hero/Programs/Program';
+import Reasons from './compoments/Hero/Reasons/Reasons';
 
 function App() {
   return (
     <div className="App">
      <Hero />
      <Program />
+     <Reasons />
     
     </div>
   );
