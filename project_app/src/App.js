99c86126dd20f989ca0727a28b6151cc12b75449
19MH1A0511 <chittidiganesh999@gmail.com>
2024-01-08 12:12:58 +0530

    commit

--- project_app/src/App.js
+++ project_app/src/App.js
@@ -4,6 +4,8 @@ import Hero from './compoments/Hero/Hero';
 import Header from './compoments/Hero/Header/Header';
 import Program from './compoments/Hero/Programs/Program';
 import Reasons from './compoments/Hero/Reasons/Reasons';
+import Plans from './compoments/Hero/Plans/Plans';
+import Testimonials from './compoments/Hero/Testimonials/Testimonials';
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
      <Hero />
      <Program />
      <Reasons />
-    
+     <Plans />
+    <Testimonials />
     </div>
   );
 }
